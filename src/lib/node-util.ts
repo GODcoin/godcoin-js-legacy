ce20687f277e992ad0abca4c569b2433a660d839
samrg472 <samrg472@gmail.com>
2018-04-06 10:14:38 -0700

    Double pressing ctrl-c will now terminal the process

--- src/lib/node-util.ts
+++ src/lib/node-util.ts
@@ -1,6 +1,8 @@
 import * as readline from 'readline';
 
 export function hookSigInt(callback: () => void) {
+  let force = false;
+
   if (process.platform === 'win32') {
     var rl = readline.createInterface({
       input: process.stdin
@@ -11,5 +13,18 @@ export function hookSigInt(callback: () => void) {
     });
   }
 
-  process.on('SIGINT', callback);
+  process.on('SIGINT', () => {
+    if (force) {
+      process.exit(1);
+    }
+    try {
+      console.log('Double press ctrl-c to force quit');
+      callback();
+    } finally {
+      force = true;
+      setTimeout(() => {
+        force = false;
+      }, 1000).unref();
+    }
+  });
 }
