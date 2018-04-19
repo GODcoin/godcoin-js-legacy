c24b5857cb26cc2f9dc768402e7b563794b81c51
samrg472 <samrg472@gmail.com>
2018-04-18 18:33:32 -0700

    Check to ensure the amount isn't negative when making transfers

--- src/lib/transactions/transfer.ts
+++ src/lib/transactions/transfer.ts
@@ -6,6 +6,7 @@ import {
 import { Tx, TxData, TxType } from './transaction';
 import * as ByteBuffer from 'bytebuffer';
 import { PublicKey } from '../crypto';
+import * as bigInt from 'big-integer';
 import { Asset } from '../asset';
 import * as assert from 'assert';
 
@@ -37,6 +38,8 @@ export class TransferTx extends Tx {
   validate(): void {
     assert(this.data.signatures.length === 1, 'tx must have 1 signature');
     assert.equal(this.data.amount.symbol, this.data.fee.symbol, 'fee must be paid with the same asset');
+    assert(this.data.amount.geq(new Asset(bigInt(0), 0, this.data.amount.symbol)), 'amount must be greater than or equal to zero');
+    assert(this.data.timestamp.getTime() < Date.now(), 'timestamp cannot be in the future');
     assert(this.data.fee.amount.gt(0), 'fee must be greater than zero');
     if (this.data.memo) {
       assert(this.data.memo.length <= 512, 'maximum memo length is 512 bytes');
