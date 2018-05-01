797602bee2574845573a0607ac3524bbb6488f54
samrg472 <samrg472@gmail.com>
2018-05-01 11:55:14 -0700

    Initialize GODCOIN_HOME with a direct function
    Signed-off-by: samrg472 <samrg472@gmail.com>

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,16 +1,14 @@
 import * as path from 'path';
 
-let APP_DIR: string;
-if (process.env.GODCOIN_DIR) {
-  APP_DIR = process.env.GODCOIN_DIR;
-} else if (process.platform === 'win32') {
-  APP_DIR = path.join(process.env.APPDIR!, '.godcoin');
-} else {
-  APP_DIR = path.join(process.env.HOME!, '.godcoin');
-}
-
 export namespace GODcoinEnv {
   export const GODCOIN_TRUST_PROXY = process.env.GODCOIN_TRUST_PROXY === 'true';
-  export const GODCOIN_HOME = APP_DIR;
-  export const GODCOIN_RC = process.env.GODCOIN_RC || path.join(APP_DIR, 'godcoinrc');
+  export const GODCOIN_HOME = (() => {
+    if (process.env.GODCOIN_DIR) {
+      return process.env.GODCOIN_DIR;
+    } else if (process.platform === 'win32') {
+      return path.join(process.env.APPDIR!, '.godcoin');
+    }
+    return path.join(process.env.HOME!, '.godcoin');
+  })();
+  export const GODCOIN_RC = process.env.GODCOIN_RC || path.join(GODCOIN_HOME, 'godcoinrc');
 }
