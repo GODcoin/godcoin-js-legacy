3e619452ebe50666e0510ceb5419aec636001f17
samrg472 <samrg472@gmail.com>
2018-05-04 14:46:26 -0700

    Make GODCOIN_HOME consistent

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,8 +3,8 @@ import * as path from 'path';
 export namespace GODcoinEnv {
   export const GODCOIN_TRUST_PROXY = process.env.GODCOIN_TRUST_PROXY === 'true';
   export const GODCOIN_HOME = (() => {
-    if (process.env.GODCOIN_DIR) {
-      return process.env.GODCOIN_DIR;
+    if (process.env.GODCOIN_HOME) {
+      return process.env.GODCOIN_HOME;
     } else if (process.platform === 'win32') {
       return path.join(process.env.APPDIR!, '.godcoin');
     }
