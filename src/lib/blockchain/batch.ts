cf4984776910a5c831dccb03f5aef21d30e7433b
samrg472 <samrg472@gmail.com>
2018-05-09 13:12:48 -0700

    Lock indexing and flushing during batch indexes

--- src/lib/blockchain/batch.ts
+++ src/lib/blockchain/batch.ts
@@ -4,9 +4,11 @@ import { ChainStore } from './chain_store';
 import { AssetSymbol } from '../asset';
 import { SignedBlock } from './block';
 import * as Long from 'long';
+import { Lock } from '../lock';
 
 export class BatchIndex {
 
+  private readonly lock = new Lock();
   private head: SignedBlock|undefined;
   private balances: BalanceMap;
   private ops: any[] = [];
@@ -18,42 +20,50 @@ export class BatchIndex {
   }
 
   async index(block: SignedBlock, bytePos?: number) {
-    if (this.head) block.validate(this.head);
-    this.head = block;
-    await this.balances.update(this.head);
-    if (bytePos !== undefined) {
-      const buf = Buffer.allocUnsafe(8);
-      buf.writeInt32BE(this.head.height.high, 0, true);
-      buf.writeInt32BE(this.head.height.low, 4, true);
+    await this.lock.lock();
+    try {
+      if (this.head) block.validate(this.head);
+      this.head = block;
 
-      const val = Long.fromNumber(bytePos, true);
-      const pos = Buffer.allocUnsafe(8);
-      pos.writeInt32BE(val.high, 0, true);
-      pos.writeInt32BE(val.low, 4, true);
+      await this.balances.update(block);
+      if (bytePos !== undefined) {
+        const buf = Buffer.allocUnsafe(8);
+        buf.writeInt32BE(block.height.high, 0, true);
+        buf.writeInt32BE(block.height.low, 4, true);
 
-      this.ops.push({
-        type: 'put',
-        key: Buffer.concat([IndexProp.NAMESPACE_BLOCK, buf]),
-        value: pos
-      });
-      if (this.ops.length >= 1000) {
-        await new Promise<void>((res, rej) => {
-          const batch = this.indexer.db.db.batch(this.ops, err => {
-            if (err) return rej(err);
-            res();
-          });
+        const val = Long.fromNumber(bytePos, true);
+        const pos = Buffer.allocUnsafe(8);
+        pos.writeInt32BE(val.high, 0, true);
+        pos.writeInt32BE(val.low, 4, true);
+
+        this.ops.push({
+          type: 'put',
+          key: Buffer.concat([IndexProp.NAMESPACE_BLOCK, buf]),
+          value: pos
         });
-        this.ops.length = 0;
+        if (this.ops.length >= 1000) await this.flushOps();
+      } else {
+        await this.store.write(block);
       }
-    } else {
-      await this.store.write(block);
-    }
-    if (this.head.height.mod(1000).eq(0)) {
-      console.log('=> Indexed block:', this.head.height.toString());
+      if (block.height.mod(1000).eq(0)) {
+        console.log('=> Indexed block:', block.height.toString());
+      }
+    } finally {
+      this.lock.unlock();
     }
   }
 
   async flush() {
+    await this.lock.lock();
+    try {
+      await this.flushOps();
+      await this.balances.write();
+    } finally {
+      this.lock.unlock();
+    }
+  }
+
+  private async flushOps() {
     if (this.ops.length) {
       await new Promise<void>((res, rej) => {
         const batch = this.indexer.db.db.batch(this.ops, err => {
@@ -63,6 +73,5 @@ export class BatchIndex {
       });
       this.ops.length = 0;
     }
-    await this.balances.write();
   }
 }
