ec61a5daf209be143cf73696bedfd7a88a272db0
samrg472 <samrg472@gmail.com>
2018-07-27 10:43:46 -0700

    Add default toolchain to rustup

--- .travis/install-rust.sh
+++ .travis/install-rust.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-if [ ! -f "$HOME/.cargo/bin/rustc" ]; then
+if [ ! -f "$HOME/.cargo/bin/rustup" ]; then
   wget https://sh.rustup.rs -O rustup-init.sh
   chmod +x rustup-init.sh
-  ./rustup-init.sh -y --no-modify-path
+  ./rustup-init.sh -y --no-modify-path --default-toolchain stable
 else
   echo 'Using cached directory.'
   rustup update
