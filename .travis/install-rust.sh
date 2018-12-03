26363f4cde6ec025088bbd20ef11050085a49238
samrg472 <samrg472@gmail.com>
2018-12-02 19:02:00 -0800

    Use rust beta as default toolchain

--- .travis/install-rust.sh
+++ .travis/install-rust.sh
@@ -4,9 +4,9 @@ set -e
 if [ ! -f "$HOME/.cargo/bin/rustup" ]; then
   wget https://sh.rustup.rs -O rustup-init.sh
   chmod +x rustup-init.sh
-  ./rustup-init.sh -y --no-modify-path --default-toolchain stable
+  ./rustup-init.sh -y --no-modify-path --default-toolchain beta
 else
   echo 'Using cached directory.'
   rustup update
-  rustup default stable
+  rustup default beta
 fi
